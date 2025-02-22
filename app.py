ddb03b3ac897082a877ce0842855d9c3ed8266f7
Yash Patil <stud22.ysp1@gec.ac.in>
2025-02-22 16:02:57 +0530

    edit

--- app.py
+++ app.py
@@ -153,7 +153,7 @@ def predict():
         X = sc.fit_transform(X)
 
         predictions = model.predict(X[0][0])
-        print(predictions)
+        print(X)
         # data = np.array(X)    
     
 
